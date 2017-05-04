932e1eae0f2fb899acd2a74f59e0e76fb2028e40
poonampatilExt <poonamp.extentia@redq.com>
2017-05-04 17:33:48 +0530

    Adding TestCommonApplication to git

---
+++ src/TestCommonApplicationForTest/TestValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCommonApplication.Controllers;
+using Xunit;
+
+namespace TestCommonApplicationForTest
+{
+    public class TestValues
+    {
+        [Fact]
+        public void TestGetMethod()
+        {
+            ValuesController valuesController = new ValuesController();
+            IEnumerable<string> result = valuesController.Get();
+
+            Assert.Equal(result.Count(), 2);
+        }
+    }
+}
