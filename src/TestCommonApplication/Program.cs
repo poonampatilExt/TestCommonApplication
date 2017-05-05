97530109afb69a3a0b014b060f500953c4873f15
EXTENTIA05\Poonam.Patil <poonampatilextentia@gmail.com>
2017-05-05 12:26:06 +0530

    localhost url changes

--- src/TestCommonApplication/Program.cs
+++ src/TestCommonApplication/Program.cs
@@ -14,6 +14,7 @@ namespace TestCommonApplication
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
